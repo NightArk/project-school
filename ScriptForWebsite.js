3412e9abdd495d3a37e698e77b1fb2cff5c432a2
Mohammad Ali <Arkhamnight278@gmail.com>
2023-12-14 07:37:17 +0300

    all file link to one css

--- ScriptForWebsite.js
+++ ScriptForWebsite.js
@@ -1,34 +1,38 @@
+var firstName,lastName,arrows,paragraph,greeting;
 function submitForm() {
     var form = document.getElementById("registrationForm");
     if (form.checkValidity()) {
-        // Form is valid, show confirmation message
+        // Form is valid show confirmation message
         document.getElementById("confirmationMessage").style.display = "block";
         document.getElementById("errorMessage").style.display = "none";
-        var firstName = form.elements["firstName"].value;
-        var lastName = form.elements["lastName"].value;
+        firstName = form.elements["firstName"].value;
+        lastName = form.elements["lastName"].value;
         localStorage.setItem('firstName', firstName);
         localStorage.setItem('lastName', lastName);
-        location.reload();
+        setTimeout(function () {
+            location.reload();
+        }, 5000);
     } else {
-        // Form is invalid, show error message
+        // Form is invalid show error message
         document.getElementById("confirmationMessage").style.display = "none";
         document.getElementById("errorMessage").style.display = "block";
     }
 }
 
+//function for expandble paragraphs
 window.onload = function () {
-    var arrows = document.querySelectorAll('.arrow');
+    arrows = document.querySelectorAll('.arrow');
 
     arrows.forEach(function (arrow) {
         arrow.addEventListener('click', function () {
-            var paragraph = this.nextElementSibling;
+             paragraph = this.nextElementSibling;
             paragraph.classList.toggle('expanded');
         });
     });
 
-    var firstName = localStorage.getItem('firstName');
-    var lastName = localStorage.getItem('lastName');
-    var greeting = document.getElementById('greeting');
+    firstName = localStorage.getItem('firstName');
+    lastName = localStorage.getItem('lastName');
+    greeting = document.getElementById('greeting');
     greeting.textContent = 'Hello,';
     if (firstName && lastName) {
         greeting.textContent += ' ' + firstName + ' ' + lastName;
